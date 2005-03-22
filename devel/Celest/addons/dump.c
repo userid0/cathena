59ba74dc0f250f399af0b09c118746bec1c8157e
celest <celest@56e55576-19cc-df11-9769-000c294b88e1>
2005-03-22 10:27:55 +0000

    Addons update

--- devel/Celest/addons/dump.c
+++ devel/Celest/addons/dump.c
@@ -4,28 +4,32 @@
 #define __ADDON
 #include <config.h>
 
-#ifdef __CYGWIN
-	// cygwin doesn't have backtrace support!
-	// is there anyway to test if execinfo.h exists?
-#else
-
 #include <stdio.h>
 #include <signal.h>
 #include <string.h>
-#include <execinfo.h>
+#ifndef __CYGWIN
+	#include <execinfo.h>
+#endif
 
 #include "../common/addons.h"
 #include "../common/addons_exports.h"
 
+#ifdef __CYGWIN
+	#define FOPEN_ freopen
+	extern void cygwin_stackdump();
+#else
+	#define FOPEN_(fn,m,s) fopen(fn,m)
+#endif
+static void sig_dump(int);
+extern const char *strsignal(int);
+
 DLLFUNC ModuleHeader MOD_HEADER(dump) = {
 	"StackDump",
-	"$Id: dump.c 0.1a",
+	"$Id: dump.c 0.2",
 	"Creates a stack dump upon crash",
 	MOD_VERSION,
 	ADDONS_MAP
 };
-
-static void sig_dump(int);
 
 // by Gabuzomeu
 // This is an implementation of signal() using sigaction() for portability.
@@ -58,11 +62,7 @@ sigfunc *compat_signal(int signo, sigfunc *func)
 #endif
 
 DLLFUNC int MOD_TEST(dump)() {
-	#ifdef __CYGWIN
-		return MOD_FAILED;
-	#else
-		return MOD_SUCCESS;
-	#endif
+	return MOD_SUCCESS;	
 }
 
 DLLFUNC int MOD_INIT(dump)(void *ct) {
@@ -74,7 +74,6 @@ DLLFUNC int MOD_INIT(dump)(void *ct) {
 }
 
 DLLFUNC int MOD_LOAD(dump)() {
-	printf ("load signals\n");
 	compat_signal(SIGSEGV, sig_dump);
 	compat_signal(SIGFPE, sig_dump);
 	compat_signal(SIGILL, sig_dump);
@@ -86,41 +85,52 @@ DLLFUNC int MOD_LOAD(dump)() {
 }
 
 DLLFUNC int MOD_UNLOAD(dump)() {
-	printf ("unload\n");
 	return MOD_SUCCESS;
 }
 
 void sig_dump(int sn)
 {
 	FILE *fp;
-	void* array[20];
-	char **stack;
-	size_t size;		
+	char file[256];
 	int no = 0;
-	char tmp[256];
+	#ifndef __CYGWIN
+		void* array[20];
+		char **stack;
+		size_t size;
+	#endif
 
 	printf ("dumping...!");
 
 	// search for a usable filename
 	do {
-		sprintf(tmp,"log/stackdump_%04d.txt", ++no);
-	} while((fp = fopen(tmp,"r")) && (fclose(fp), no < 9999));
+		sprintf (file, "log/stackdump_%04d.txt", ++no);
+	} while((fp = fopen(file,"r")) && (fclose(fp), no < 9999));
 	// dump the trace into the file
-	if ((fp = fopen (tmp,"w")) != NULL) {
-		fprintf(fp,"Exception: %s\n", strsignal(sn));
-		fprintf(fp,"Stack trace:\n");
+
+	if ((fp = FOPEN_(file, "w", stderr)) != NULL) {
+		printf ("Dumping stack... ");
+		fprintf(fp, "Exception: %s \n", strsignal(sn));
+		fflush (fp);
+
+	#ifdef __CYGWIN
+		cygwin_stackdump ();
+	#else
+		fprintf(fp, "Stack trace:\n");
 		size = backtrace (array, 20);
 		stack = backtrace_symbols (array, size);
 		for (no = 0; no < size; no++) {
 			fprintf(fp, "%s\n", stack[no]);
 		}
 		fprintf(fp,"End of stack trace\n");
-		fclose(fp);
 		FREE(stack);
+	#endif
+
+		printf ("Done.\n");
+		fflush(stdout);
+		fclose(fp);
 	}
 	// Pass the signal to the system's default handler
 	compat_signal(sn, SIG_DFL);
 	raise(sn);
 }
 
-#endif
